90975fb126ac47ae07c38dff70048c63af2ba062
MarioTiara <mario.tiarapratama@gmail.com>
2022-10-18 22:17:49 +0700

    min insert to be palindrome

--- LCS/Program.cs
+++ LCS/Program.cs
@@ -7,50 +7,27 @@ namespace LCS
     {
         static void Main(string[] args)
         {
-            string A = "bd";
-            string B="abcd";
+            string A = "AGGTAB";
+            string B="GXTXAYB";
             Console.WriteLine(Lcs(A,B,A.Length,B.Length));
         }
 
         public static int Lcs(string X, string Y, int m, int n)
         {
-        int[] prev = new int[n + 1];
-        int[] curr = new int[n + 1];
-        int i, j;
- 
-        for (i = 0; i <= m; i++) {
-            for (j = 0; j <= n; j++) {
-                if (i == 0 || j == 0)
-                    prev[j] = 0;
- 
-                else if (X[i - 1] == Y[j - 1])
-                    curr[j] = prev[j - 1] + 1;
- 
-                else
-                    curr[j]
-                        = Math.Max(prev[j], curr[j - 1]);
+
+            int [,] LCS= new int[m+1, n+1];
+            for(int i=0; i<=m; i++){
+                for(int j=0; j<=n; j++){
+                    if (i==0 || j==0) LCS[i,j]=0;
+                    else if (X[i-1]==Y[j-1]) LCS[i,j]=LCS[i-1, j-1]+1;
+                    else LCS[i,j]=Math.Max(LCS[i-1,j], LCS[i,j-1]);
+                }
+
             }
- 
-            prev = curr;
-            }
- 
-        /* L[m][n] contains length of LCS for X[0..n-1]
-                and Y[0..m-1] */
-            return prev[n];
-    }
- 
-    // LCS based function to find minimum number of
-    // insertions
-    static int FindMinInsertionsLCS(string str, int n)
-    {
-        // Creat another string to store reverse of 'str'
-        char[] tmp = str.ToCharArray();
-        Array.Reverse(tmp);
-        string rev = new string(tmp);
- 
-        // The output is length of string minus length of
-        // lcs of str and it reverse
-        return (n - Lcs(str, rev, n, n));
+
+            return LCS[m,n];
         }
     }
+ 
+    
 }
---
+++ Minimum-Insert-Palindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Minimum_Insert_Palindrome
+{
+    class Program
+    {
+        static int Lcs(char[] X, char[] Y, int m, int n )
+        {
+
+            int [,] L= new int[m+1, n+1];
+            for (int i = 0; i <= m; i++) 
+            { 
+                for (int j = 0; j <= n; j++) 
+                { 
+                    if (i == 0 || j == 0) 
+                        L[i, j] = 0; 
+                    else if (X[i - 1] == Y[j - 1]) 
+                        L[i, j] = L[i - 1, j - 1] + 1; 
+                    else
+                        L[i, j] = Math.Max(L[i - 1, j], L[i, j - 1]); 
+                } 
+            }
+
+            return L[m,n];
+        }
+
+        static int minInsertions(string input) {
+            char[] X=input.ToCharArray(); 
+            char[] Y=input.ToCharArray(); 
+            Array.Reverse(Y);
+            return input.Length-Lcs(X,Y,X.Length,Y.Length);
+        }
+        static void Main(string[] args)
+        {
+            string s1 = "mbadm"; 
+            Console.WriteLine(minInsertions(s1));
+        }
+
+
+     
+
+        
+
+        
+    }
+}
